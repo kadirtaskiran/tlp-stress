26b59008fbbf9b2ae0a0796b532ac9b3d1dbe0a3
Kadir Taşkıran <kadir@cloudnesil.com>
2019-12-10 16:42:08 +0300

    additional field added

--- src/main/kotlin/com/thelastpickle/tlpstress/profiles/KeyValue.kt
+++ src/main/kotlin/com/thelastpickle/tlpstress/profiles/KeyValue.kt
@@ -18,16 +18,17 @@ class KeyValue : IStressProfile {
 
 
     override fun prepare(session: Session) {
-        insert = session.prepare("INSERT INTO keyvalue (key, value) VALUES (?, ?)")
-        select = session.prepare("SELECT * from keyvalue WHERE key = ? and value = ?")
-        delete = session.prepare("DELETE from keyvalue WHERE key = ? and value = ?")
+        insert = session.prepare("INSERT INTO keyvalue (key, datasource_partition, value) VALUES (?, ?, ?)")
+        select = session.prepare("SELECT * from keyvalue WHERE key = ? and datasource_partition = ? and value = ?")
+        delete = session.prepare("DELETE from keyvalue WHERE key = ? and datasource_partition = ? and value = ?")
     }
 
     override fun schema(): List<String> {
         val table = """CREATE TABLE IF NOT EXISTS keyvalue (
                         key text,
+                        datasource_partition int,
                         value text,
-                        PRIMARY KEY (key, value)
+                        PRIMARY KEY ((key, datasource_partition),value)
                         )""".trimIndent()
         return listOf(table)
     }
@@ -45,21 +46,21 @@ class KeyValue : IStressProfile {
             override fun getNextSelect(partitionKey: PartitionKey): Operation {
                 val data = value.getText()
 
-                val bound = select.bind("foo$data", data )
+                val bound = select.bind("foo", data.hashCode()%256 , data  )
                 return Operation.SelectStatement(bound)
             }
 
             override fun getNextMutation(partitionKey: PartitionKey): Operation {
                 val data = value.getText()
                 //val bound = insert.bind(partitionKey.getText(),  data)
-                val bound = insert.bind("foo$data",  data )
+                val bound = insert.bind("foo", data.hashCode()%256 , data )
 
                 return Operation.Mutation(bound)
             }
 
             override fun getNextDelete(partitionKey: PartitionKey): Operation {
                 val data = value.getText()
-                val bound = delete.bind("foo$data", data)
+                val bound = delete.bind("foo", data.hashCode()%256 , data )
                 return Operation.Deletion(bound)
             }
         }
